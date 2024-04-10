4b7be0fd65129bfdb8d6c43299b07342f69e815c
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-04-10 19:30:10 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,45 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Footer from "./components/footer/page";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div className={styles.welcomeContainer}>
-				<Image
-					src="/logo.png"
-					width={300}
-					height={300}
-					alt="Logo"
-					priority
-				></Image>
-				<p>
-					Hey there, welcome to XerStream. We are excited to have you
-					on board.
-				</p>
+			<div className={styles.content}>
+				<div className={styles.contentContainer}>
+					<div className={styles.manga}>
+						<Link href={"/manga"}>
+							<h2>Manga</h2>
+							<p>Your one stop for all your manga needs</p>
+						</Link>
+					</div>
+					<div className={styles.anime}>
+						<Link href={"/anime"}>
+							<h2>Anime</h2>
+							<p>Your one stop for all your anime needs</p>
+						</Link>
+					</div>
+					<div className={styles.kdrama}>
+						<Link
+							href={"/kdrama"}
+							style={{ pointerEvents: "none" }}
+						>
+							<h2>Kdrama</h2>
+							<p>Your one stop for all your kdrama needs</p>
+						</Link>
+					</div>
+				</div>
 			</div>
-			<Footer />
+			<div className={styles.netlifyLogo}>
+				<Link href={"https://dramalama.netlify.app"} target="_blank">
+					<Image
+						src={"/netlify.svg"}
+						width={300}
+						height={130}
+						alt="Netlify Logo"
+					/>
+				</Link>
+			</div>
 		</main>
 	);
 }
