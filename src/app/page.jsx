aace86d772188cc120567bd3bcf6cc0f1404cd0e
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-04-10 19:37:50 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,10 +20,7 @@ export default function Home() {
 						</Link>
 					</div>
 					<div className={styles.kdrama}>
-						<Link
-							href={"/kdrama"}
-							style={{ pointerEvents: "none" }}
-						>
+						<Link href={"/kdrama"}
 							<h2>Kdrama</h2>
 							<p>Your one stop for all your kdrama needs</p>
 						</Link>
